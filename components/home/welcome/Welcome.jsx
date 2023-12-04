9d4a2689bd09e611079dac4d07ac937b79c5fd0b
FredrickBarrett <f.barrett12@gmail.com>
2023-12-03 19:20:02 -0500

    let me save this before I continue to fuck this up

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { useState } from 'react'
+import { 
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList 
+} from 'react-native'
+import { useRouter } from 'expo-router'
 
 import styles from './welcome.style'
+import { icons, SIZES } from '../../../constants'
 
 const Welcome = () => {
+  const router = useRouter()
+
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Fredrick</Text>
+        <Text style={styles.welcomeMessage}>Welcome to JobLobby</Text>
+      </View>
+
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput 
+            style={styles.searchInput}
+            value=""
+            placeholder="What are you looking for?"
+            onChange={() => {}}
+          />
+        </View>
+      </View>
     </View>
   )
 }
